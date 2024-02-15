a63bd4a307cb4b3453d059b589330dcddad6dde8
edRibeiro <edersonribeiro.ita@gmail.com>
2024-02-15 02:49:09 -0300

    feat: create migrations

--- database/migrations/2024_02_15_051808_create_addresses_table.php
+++ database/migrations/2024_02_15_051808_create_addresses_table.php
@@ -11,12 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('street');
+            $table->string('number');
+            $table->string('complement');
+            $table->string('neighborhood');
+            $table->foreignId('city_id')
+                ->constrained(table: 'cities', indexName: 'addresses_city_id')
+                ->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -27,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('addresses');
     }
 };
