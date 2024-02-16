c9db48f522e5bc6f342a3a0ff2409ef53084dc43
edRibeiro <edersonribeiro.ita@gmail.com>
2024-02-15 23:37:17 -0300

    feat: Adding business logic in user service.

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,38 +2,96 @@
 
 namespace App\Services;
 
+use App\Dtos\AddressDto;
+use App\Dtos\CityDto;
+use App\Dtos\StateDto;
+use App\Dtos\UserDto;
 use App\Models\User;
+use App\Repositories\Contracts\AddressRepositoryInterface;
+use App\Repositories\Contracts\CityRepositoryInterface;
+use App\Repositories\Contracts\StateRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Services\Contracts\UserServiceInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserService implements UserServiceInterface
 {
-  public function __construct(protected UserRepositoryInterface $repository)
-  {
+  public function __construct(
+    protected UserRepositoryInterface $userRepository,
+    protected AddressRepositoryInterface $addressRepository,
+    protected CityRepositoryInterface $cityRepository,
+    protected StateRepositoryInterface $stateRepository,
+  ) {
   }
 
   public function findAll(): array
   {
-    return $this->repository->findAll();
+    return $this->userRepository->getAll();
   }
 
   public function findOne(int $id): User|null
   {
-    return $this->repository->findOne($id);
+    return $this->userRepository->findOne($id);
   }
 
-  public function new($dto): User
+  public function new(UserDto $dto): User
   {
-    return $this->repository->new($dto);
+    $stateData = $this->stateRepository->new($dto->address->city->state);
+    $dto->address->city->state = new StateDto($stateData->name, $stateData->id);
+    $cityData = $this->cityRepository->new($dto->address->city);
+    $dto->address->city = new CityDto(
+      $cityData->name,
+      $dto->address->city->state,
+      $cityData->id
+    );
+    $addressData = $this->addressRepository->new($dto->address);
+    $dto->address = new AddressDto(
+      $addressData->street,
+      $addressData->number,
+      $addressData->complement,
+      $addressData->neighborhood,
+      $dto->address->city,
+      $addressData->id
+    );
+    return $this->userRepository->new($dto);
   }
 
   public function update($dto, int $id): User|null
   {
-    return $this->repository->update($dto, $id);
+    $userData = $this->findOne($id);
+    if (!$userData) {
+      throw new NotFoundHttpException();
+    }
+    $stateData = $this->stateRepository->findByName($dto->address->city->state->name);
+    if (!$stateData) {
+      $stateData = $this->stateRepository->new($dto->address->city->state->name);
+    }
+    $dto->address->city->state = new StateDto($stateData->name, $stateData->id);
+    $cityData = $this->cityRepository->findByName($dto->address->city->name);
+    if (!$cityData) {
+      $cityData = $this->cityRepository->new($dto->address->city);
+    }
+    $dto->address->city = new CityDto(
+      $cityData->name,
+      $dto->address->city->state,
+      $cityData->id
+    );
+    $addressData = $this->addressRepository->update($dto->address, $userData->address_id);
+    $dto->address = new AddressDto(
+      $addressData->street,
+      $addressData->number,
+      $addressData->complement,
+      $addressData->neighborhood,
+      $dto->address->city,
+      $addressData->id
+    );
+    return $this->userRepository->update($dto, $id);
   }
 
   public function delete(int $id): void
   {
-    $this->repository->delete($id);
+    $user = self::findOne($id);
+    $this->addressRepository->delete($user->address_id);
+    $this->userRepository->delete($id);
   }
 }
